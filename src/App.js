7bd89326668ef365223c1d289ef23b6ad5ae404c
Chelysita <ara.medinago@gmail.com>
2023-10-02 11:16:57 -0400

    Files of search weather added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createRoot } from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <index />
+        <h1>Weather App</h1>
+
+        <WeatherSearch />
+        <a href="https://github.com/Chelysita/week_4_hmw.git">
+          Code shared on Github
         </a>
       </header>
     </div>
